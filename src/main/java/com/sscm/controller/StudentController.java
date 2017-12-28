8a3691480b2249cabfe44a78bfae1fc9859b78fd
sock <huangshaojie@youmi.net>
2017-12-28 18:37:23 +0800

    add

--- src/main/java/com/sscm/controller/StudentController.java
+++ src/main/java/com/sscm/controller/StudentController.java
@@ -1,8 +1,18 @@
 package com.sscm.controller;
 
+
+import org.springframework.stereotype.Controller;
+
+
 /**
  * Created by sock on 17-12-4.
  */
 
+@Controller
 public class StudentController {
+    public static void main(String args[]){
+        System.out.println("Hello");
+    }
+
+
 }
